64290e386707bc5fde36c75d7810c088bca080be
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-06-18 16:48:16 +0200

    Adding vulnerable machine, blocked function and blocked function error

--- pylo/__init__.py
+++ pylo/__init__.py
@@ -1,2 +1,6 @@
 from .event import Event
+from .events import emergency
 from .measurement_variable import MeasurementVariable
+from .blocked_function import BlockedFunction
+from .blocked_function_error import BlockedFunctionError
+from .vulnerable_machine import VulnerableMachine
---
+++ pylo/blocked_function_error.py
@@ -0,0 +1,5 @@
+class BlockedFunctionError(LookupError):
+    """This class represents an error when a function is called, that is 
+    currently blocked.
+    """
+    pass
