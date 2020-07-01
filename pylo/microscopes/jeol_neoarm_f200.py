496e35c515ad000b3260d29a4f9bba775f410bb7
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-07-01 15:31:50 +0200

    Adding hint for implementation

--- pylo/microscopes/jeol_neoarm_f200.py
+++ pylo/microscopes/jeol_neoarm_f200.py
@@ -2,3 +2,7 @@ from .microscope_interface import MicroscopeInterface
 
 class JEOLNeoARMF200(MicroscopeInterface):
     pass
+
+# make sure to somehow save when a measurement variable is set, in the setsafemode
+# wait until the measurement variable is set successfully, then set the 
+# safe mode, use threading.Lock or queue.SimpleQueue
