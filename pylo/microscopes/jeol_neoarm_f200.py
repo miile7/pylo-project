b6a858ac45c749b5ec1295b905e1f06924af0eec
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-07-01 16:09:06 +0200

    WIP: Creating constructor of jeol neoarm

--- pylo/microscopes/jeol_neoarm_f200.py
+++ pylo/microscopes/jeol_neoarm_f200.py
@@ -1,7 +1,24 @@
 from .microscope_interface import MicroscopeInterface
+from ..measurement_variable import MeasurementVariable
 
 class JEOLNeoARMF200(MicroscopeInterface):
-    pass
+    """This class is the interface for communicating with the JEOL NeoARM F200
+    TEM.
+    """
+    
+    def __init__(self):
+        """Get the microscope instance"""
+        super().__init__()
+
+        # set all measurement variables sequential, not parallel
+        self.supports_parallel_measurement_variable_setting = False
+
+        self.supported_measurement_variables = [
+            MeasurementVariable("focus", "Focus", 0, 1, "hex"),
+            MeasurementVariable("ol-current", "Objective lense current", 0, 1, "hex"),
+            MeasurementVariable("x-tilt", "X Tilt", -10, 10, "deg"),
+            MeasurementVariable("y-tilt", "Y Tilt", -10, 10, "deg"),
+        ]
 
 # make sure to somehow save when a measurement variable is set, in the setsafemode
 # wait until the measurement variable is set successfully, then set the 
