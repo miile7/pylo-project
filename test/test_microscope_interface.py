78d3f751d4c71426a5d3e1312f2a7a81894ccecd
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-07-07 09:33:07 +0200

    Adding controller to MicroscopeInterface, adding comments to test

--- test/test_microscope_interface.py
+++ test/test_microscope_interface.py
@@ -1,10 +1,15 @@
 import pytest
-import pylo.microscopes
+
 import pylo
+import pylo.microscopes
+
+class DummyController(pylo.Controller):
+    def __init__(self):
+        pass
 
 class TestMicroscopeInterface:
     def setup_method(self, method):
-        self.microscope = pylo.microscopes.MicroscopeInterface()
+        self.microscope = pylo.microscopes.MicroscopeInterface(DummyController())
         self.microscope.supported_measurement_variables.append(
             pylo.MeasurementVariable("test-var-1", "Test Variable 1", 0, 10, "ut")
         )
@@ -16,22 +21,32 @@ class TestMicroscopeInterface:
         )
 
     def test_error_is_thrown_set_lorenz_mode(self):
+        """Test if the setInLorenzMode() function raises a 
+        NotImplementedError."""
         with pytest.raises(NotImplementedError):
             self.microscope.setInLorenzMode(True)
         
     def test_error_is_thrown_get_lorenz_mode(self):
+        """Test if the getInLorenzMode() function raises a 
+        NotImplementedError."""
         with pytest.raises(NotImplementedError):
             self.microscope.getInLorenzMode()
         
     def test_error_is_thrown_set_measurement_variable(self):
+        """Test if the setMeasurementVariableValue() function raises a 
+        NotImplementedError."""
         with pytest.raises(NotImplementedError):
             self.microscope.setMeasurementVariableValue("test-variable", 0)
         
     def test_error_is_thrown_get_measurement_variable(self):
+        """Test if the getMeasurementVariableValue() function raises a 
+        NotImplementedError."""
         with pytest.raises(NotImplementedError):
             self.microscope.getMeasurementVariableValue("test-variable")
         
     def test_error_is_thrown_reset_to_safe_state(self):
+        """Test if the resetToSafeState() function raises a 
+        NotImplementedError."""
         with pytest.raises(NotImplementedError):
             self.microscope.resetToSafeState()
 
@@ -46,6 +61,9 @@ class TestMicroscopeInterface:
         ("test-var-3", 2),
     ])
     def test_is_valid_measurement_variable_value(self, id_, value):
+        """Test if the values of the isValidMeasurementVariableValue() function
+        returns True if the values are allowed, especially the that the min and 
+        max values are included in the allowed range."""
         assert self.microscope.isValidMeasurementVariableValue(id_, value) == True
 
     @pytest.mark.parametrize("id_,value", [
@@ -59,4 +77,6 @@ class TestMicroscopeInterface:
         ("test-var-1", "wrong-type")
     ])
     def test_is_invalid_measurement_variable_value(self, id_, value):
+        """Test if invalid values for `MeasurementVariables` return False in 
+        isValidMeasurementVariableValue()."""
         assert self.microscope.isValidMeasurementVariableValue(id_, value) == False
