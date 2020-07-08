5a6d4ff40953d2c56bfda537b52998ada046708f
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-07-08 13:05:43 +0200

    Adding test for Controller::_dynamicCreateClass()

--- test/test_controller.py
+++ test/test_controller.py
@@ -10,6 +10,8 @@ import pytest
 
 import pylo
 
+pylo_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), "pylo")
+
 # class DummyView(pylo.AbstractView):
 class DummyView:
     def __init__(self):
@@ -213,3 +215,231 @@ class TestController:
             
             # check if the returned value is correct
             assert values[i] == l["value"]
+        
+    def create_dummy_class_file(self, directory):
+        """Creates a python file that defines two classes and saves it to the 
+        given directory.
+
+        Raises
+        ------
+        OSError
+            When the file cannot be created
+
+        Parameters
+        ----------
+        directory : str
+            The directory to save the file to
+        
+        Returns
+        -------
+        str
+            The file name
+        str
+            The full file path (including the file name)
+        str
+            The module name
+        str
+            The class name (the second class has the same name with a "2"
+            appended)
+        """
+
+        class_name = "ControllerTestDummyClass{}C".format(random.randint(0, 99999999))
+        module = class_name.lower()
+        filename = module + ".py"
+
+        os.makedirs(directory, exist_ok=True)
+        path = os.path.join(directory, filename)
+
+        f = open(path, "w+")
+        f.write(("class {cn}:\n" + 
+                    "\tdef __init__(self, *args):\n" + 
+                        "\t\tself.dummy_value = \"dummy value\"\n" + 
+                        "\t\tself.constructor_args = args\n"
+                "\n" + 
+                    "\tdef dummy_func(self):\n" + 
+                        "\t\treturn True\n"
+                "\n" + 
+                "\n" + 
+                "class {cn}2:\n" + 
+                    "\tdef __init__(self, *args):\n" + 
+                        "\t\tself.constructor_args = args\n" + 
+                "\n" + 
+                "\n" + 
+                "thisisnotaclass = True").format(cn=class_name))
+        f.close()
+
+        return filename, path, module, class_name
+    
+    def remove_file_and_dir(self, file_name):
+        """Removes the given file and the parent directory if the directory
+        only contained this file."""
+
+        parent_dir = os.path.dirname(file_name)
+        os.remove(file_name)
+
+        if len(os.listdir(parent_dir)) == 0:
+            os.removedirs(parent_dir)
+    
+    def check_dynamic_created_object(self, obj, class_name, constructor_args=None):
+        """Check if the `obj` is created correctly."""
+
+        assert isinstance(obj, object)
+        assert hasattr(obj, "__class__")
+        assert hasattr(obj.__class__, "__name__")
+        assert obj.__class__.__name__ == class_name
+
+        if constructor_args is not None:
+            assert obj.constructor_args == constructor_args
+    
+    @pytest.mark.usefixtures("controller")
+    def test_dynamic_create_class_in_root(self, controller):
+        """Test whether the _dynamicCreateClass() function works for a file
+        in the pylo root directory."""
+
+        filename, path, module, class_name = self.create_dummy_class_file(pylo_root)
+
+        controller.configuration.setValue("setup", "dummy-test-module-name", module)
+        controller.configuration.setValue("setup", "dummy-test-class-name", class_name)
+
+        obj = controller._dynamicCreateClass("dummy-test-module-name", 
+                                             "dummy-test-class-name")
+        
+        self.check_dynamic_created_object(obj, class_name)
+        
+        self.remove_file_and_dir(path)
+    
+    @pytest.mark.usefixtures("controller")
+    def test_dynamic_create_class_in_root_with_args(self, controller):
+        """Test whether the _dynamicCreateClass() function works with 
+        constructor arguments."""
+
+        filename, path, module, class_name = self.create_dummy_class_file(pylo_root)
+
+        controller.configuration.setValue("setup", "dummy-test-module-name", module)
+        controller.configuration.setValue("setup", "dummy-test-class-name", class_name)
+
+        args = ("A", "B", "C")
+        obj = controller._dynamicCreateClass("dummy-test-module-name", 
+                                             "dummy-test-class-name",
+                                             constructor_args=args)
+        
+        self.check_dynamic_created_object(obj, class_name, args)
+        
+        self.remove_file_and_dir(path)
+    
+    @pytest.mark.usefixtures("controller")
+    def test_dynamic_create_class_in_root_with_args_and_class_2(self, controller):
+        """Test whether the _dynamicCreateClass() function works with two 
+        classes loaded form the same source."""
+
+        filename, path, module, class_name = self.create_dummy_class_file(pylo_root)
+        class_name2 = class_name + "2"
+
+        controller.configuration.setValue("setup", "dummy-test-module-name", module)
+        controller.configuration.setValue("setup", "dummy-test-class-name", class_name)
+
+        controller.configuration.setValue("setup", "dummy-test-module-name2", module)
+        controller.configuration.setValue("setup", "dummy-test-class-name2", class_name2)
+
+        args1 = ("A", "B", "C")
+        obj1 = controller._dynamicCreateClass("dummy-test-module-name", 
+                                             "dummy-test-class-name",
+                                             constructor_args=args1)
+        args2 = ("D", "E", "F")
+        obj2 = controller._dynamicCreateClass("dummy-test-module-name2", 
+                                             "dummy-test-class-name2",
+                                             constructor_args=args2)
+        
+        self.check_dynamic_created_object(obj1, class_name, args1)
+        self.check_dynamic_created_object(obj2, class_name2, args2)
+        
+        self.remove_file_and_dir(path)
+
+    @pytest.mark.usefixtures("controller")
+    def test_dynamic_create_class_in_submodule(self, controller):
+        """Test whether the _dynamicCreateClass() function works for a file
+        in the micrsocopes subdirectory."""
+
+        path = os.path.join(pylo_root, "microscopes")
+        filename, path, module, class_name = self.create_dummy_class_file(path)
+
+        controller.configuration.setValue("setup", "dummy-test-module-name", module)
+        controller.configuration.setValue("setup", "dummy-test-class-name", class_name)
+
+        obj = controller._dynamicCreateClass("dummy-test-module-name", 
+                                             "dummy-test-class-name")
+        
+        self.check_dynamic_created_object(obj, class_name)
+        
+        self.remove_file_and_dir(path)
+
+    @pytest.mark.usefixtures("controller")
+    def test_dynamic_create_class_in_cwd(self, controller):
+        """Test whether the _dynamicCreateClass() function works for a file
+        in the current working directory."""
+
+        # this should be the current working directory
+        path = os.getcwd()
+        try:
+            filename, path, module, class_name = self.create_dummy_class_file(path)
+        except OSError:
+            pytest.skip("The test file {} could not be created. Cannot test " + 
+                        "importing if the file does not exist.")
+
+        controller.configuration.setValue("setup", "dummy-test-module-name", module)
+        controller.configuration.setValue("setup", "dummy-test-class-name", class_name)
+
+        obj = controller._dynamicCreateClass("dummy-test-module-name", 
+                                             "dummy-test-class-name")
+        
+        self.check_dynamic_created_object(obj, class_name)
+        
+        self.remove_file_and_dir(path)
+
+    @pytest.mark.usefixtures("controller")
+    def test_dynamic_create_class_module_does_not_exist(self, controller):
+        """Test whether the _dynamicCreateClass() function raises an exception
+        when the module does not exist."""
+
+        filename, path, module, class_name = self.create_dummy_class_file(pylo_root)
+
+        controller.configuration.setValue("setup", "dummy-test-module-name", "thisdoesnotexist")
+        controller.configuration.setValue("setup", "dummy-test-class-name", class_name)
+
+        with pytest.raises(ModuleNotFoundError):
+            controller._dynamicCreateClass("dummy-test-module-name", 
+                                           "dummy-test-class-name")
+
+        self.remove_file_and_dir(path)
+        
+    @pytest.mark.usefixtures("controller")
+    def test_dynamic_create_class_class_does_not_exist(self, controller):
+        """Test whether the _dynamicCreateClass() function raises an exception
+        when the class does not exist."""
+
+        filename, path, module, class_name = self.create_dummy_class_file(pylo_root)
+
+        controller.configuration.setValue("setup", "dummy-test-module-name", module)
+        controller.configuration.setValue("setup", "dummy-test-class-name", "thisdoesnotexist")
+
+        with pytest.raises(AttributeError):
+            controller._dynamicCreateClass("dummy-test-module-name", 
+                                           "dummy-test-class-name")
+
+        self.remove_file_and_dir(path)
+        
+    @pytest.mark.usefixtures("controller")
+    def test_dynamic_create_class_class_is_not_a_class(self, controller):
+        """Test whether the _dynamicCreateClass() function raises an exception
+        when the class is not a class."""
+
+        filename, path, module, class_name = self.create_dummy_class_file(pylo_root)
+
+        controller.configuration.setValue("setup", "dummy-test-module-name", module)
+        controller.configuration.setValue("setup", "dummy-test-class-name", "thisisnotaclass")
+
+        with pytest.raises((NameError, TypeError)):
+            controller._dynamicCreateClass("dummy-test-module-name", 
+                                           "dummy-test-class-name")
+
+        self.remove_file_and_dir(path)
