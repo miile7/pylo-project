fc24f3300dd23a2503bde2cf8a34beb4ce2bc82c
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-07-19 11:29:35 +0200

    Changing configuration to be persistant

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import pylo
 import pylo.config
 
 pylo.config.VIEW = pylo.CLIView()
-pylo.config.CONFIGURATION = pylo.AbstractConfiguration()
+pylo.config.CONFIGURATION = pylo.IniConfiguration()
 
 pylo.execute()
