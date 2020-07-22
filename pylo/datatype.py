8d8a608fcdb5c83687b2eb1da598a0f812aace21
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-07-22 15:26:21 +0200

    Fixing and improving handling of format definitions for the format string

--- pylo/datatype.py
+++ pylo/datatype.py
@@ -48,7 +48,7 @@ class Datatype:
         else:
             return value
     
-    def format(self, value: typing.Any, format: str) -> str:
+    def format(self, value: typing.Any, format: typing.Optional[str]="") -> str:
         """Parse the `value`.
 
         Parameters
@@ -97,7 +97,7 @@ class Datatype:
         tupl or None
             A tuple that represents the specification or None if the
             secification is invalid, the tuple has the following indices:
-            - 0: fill character or space if not defined
+            - 0: fill character or an empty string
             - 1: alignment character or an empty string
             - 2: sign character or an empty string
             - 3: "#" for alternative representation, empty string for normal
@@ -114,10 +114,7 @@ class Datatype:
         if matches is not None:
             pattern = []
             for i, v in enumerate(matches.groups()):
-                if i == 0 and v == "":
-                    # default alignment character is space
-                    v = " "
-                elif (i == 5 or i == 7) and v != "":
+                if (i == 5 or i == 7) and v != "":
                     if v.startswith("."):
                         v = v[1:]
                     v = int(v)
@@ -128,3 +125,48 @@ class Datatype:
             pattern = None
         
         return pattern
+    
+    @staticmethod
+    def join_format_spec(format_tuple: typing.Tuple[str, str, str, str, str, typing.Union[str, int], str, typing.Union[str, int], str], keyname: typing.Optional[str]="") -> str:
+        """Join the `format_tuple` of the form as returned by 
+        `Datatype::split_format_tuple()` to a valid format string.
+
+        This returns the format string including the curly brackets and the 
+        colon. For using a key you can set the `keyname`.
+
+        The `format_tuple` must have the following form. At index:
+        - 0: fill character or an empty string
+        - 1: alignment character or an empty string
+        - 2: sign character or an empty string
+        - 3: "#" for alternative representation, empty string for normal
+        - 4: "0" for zeros between sign and number, empty string otherwise
+        - 5: width as an int or an empty string
+        - 6: thousands grouping option ("_" or ",") or empty string for no 
+            grouping
+        - 7: precision as an int or an empty string
+        - 8: type specifier
+
+        Parameters
+        ----------
+        format_tuple : tuple
+            The format tuple of the form as the `Datatype::split_format_tuple()`
+            returns
+        keyname : str, optional
+            The name of the key for the format string, this goes before the 
+            colon (`{keyname:format_tuple}`)
+        
+        Returns
+        -------
+        str
+            A valid format string
+        """
+
+        format_tuple = list(format_tuple)
+
+        if format_tuple[8] in ("b", "c", "d", "o", "x", "X", "n"):
+            # precision is not allowed for integer types
+            format_tuple[7] = ""
+        elif format_tuple[7] != "":
+            format_tuple[7] = "." + str(format_tuple[7])
+        
+        return "{" + str(keyname) + ":" + "".join(map(str, format_tuple)) + "}"
