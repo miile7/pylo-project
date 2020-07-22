7d89eba437baf89b8852fab719f19f819d0d92d6
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-07-22 15:26:36 +0200

    Adding more tests for new Datatype functions

--- test/test_datatype.py
+++ test/test_datatype.py
@@ -12,38 +12,25 @@ import pylo
 
 @pytest.fixture()
 def hex_int_datatype():
-    def format_hex(v, f):
-        f = list(pylo.Datatype.split_format_spec(f))
-        # alternative form, this will make 0x<number>
-        f[3] = "#"
-        # convert to hex
-        f[8] = "x"
-
-        return ("{" + "".join(f) + "}").format(v)
-
-    return pylo.Datatype(
-        "hex", 
-        format_hex,
-        lambda x: x if isinstance(x, int) else int(str(x), base=16)
-    )
+    return pylo.microscopes.pyjem_microscope.hex_int
 
 class TestDatatype:
     @pytest.mark.parametrize("spec,expected", [
         # examples taken from python format doc
-        ("<30", (" ", "<", "", "", "", 30, "", "", "")),
-        (">20", (" ", ">", "", "", "", 20, "", "", "")),
+        ("<30", ("", "<", "", "", "", 30, "", "", "")),
+        (">20", ("", ">", "", "", "", 20, "", "", "")),
         ("*^10", ("*", "^", "", "", "", 10, "", "", "")),
-        ("+f", (" ", "", "+", "", "", "", "", "", "f")),
-        ("-f", (" ", "", "-", "", "", "", "", "", "f")),
-        (" f", (" ", "", " ", "", "", "", "", "", "f")),
-        ("+b", (" ", "", "+", "", "", "", "", "", "b")),
-        ("-x", (" ", "", "-", "", "", "", "", "", "x")),
-        (" o", (" ", "", " ", "", "", "", "", "", "o")),
-        ("d", (" ", "", "", "", "", "", "", "", "d")),
-        ("#x", (" ", "", "", "#", "", "", "", "", "x")),
-        ("#o", (" ", "", "", "#", "", "", "", "", "o")),
-        ("0,", (" ", "", "", "", "0", "", ",", "", "")),
-        (".2%", (" ", "", "", "", "", "", "", 2, "%")),
+        ("+f", ("", "", "+", "", "", "", "", "", "f")),
+        ("-f", ("", "", "-", "", "", "", "", "", "f")),
+        (" f", ("", "", " ", "", "", "", "", "", "f")),
+        ("+b", ("", "", "+", "", "", "", "", "", "b")),
+        ("-x", ("", "", "-", "", "", "", "", "", "x")),
+        (" o", ("", "", " ", "", "", "", "", "", "o")),
+        ("d", ("", "", "", "", "", "", "", "", "d")),
+        ("#x", ("", "", "", "#", "", "", "", "", "x")),
+        ("#o", ("", "", "", "#", "", "", "", "", "o")),
+        ("0,", ("", "", "", "", "0", "", ",", "", "")),
+        (".2%", ("", "", "", "", "", "", "", 2, "%")),
     ])
     def test_split_format_spec(self, spec, expected):
         """Test if the split format function works."""
@@ -54,16 +41,33 @@ class TestDatatype:
     @pytest.mark.parametrize("parse,expected", [
         (0, 0),
         (-10, -10),
+        (-15.5, -15),
+        # this is treated as a hex number
         ("-10", -16),
         ("-A", -10),
         ("-0xA", -10),
         ("-0XA", -10),
         ("-0xa", -10),
         ("-0xA", -10),
-        ("0xFF", 255)
+        ("0xFF", 255),
+        # this is treated as a hex number
+        ("15.5", 21)
     ])
     def test_hex_int_parse(self, hex_int_datatype, parse, expected):
         """Test if the hex int datatype works."""
 
         assert hex_int_datatype.parse(parse) == expected
         assert hex_int_datatype(parse) == expected
+    
+    @pytest.mark.usefixtures("hex_int_datatype")
+    @pytest.mark.parametrize("value,format_spec,expected", [
+        (10, "", "0xa"),
+        (0xb, "", "0xb"),
+        (0xb, "g", "0xb"),
+        (0xb, "b", "0xb"),
+        (0xb, "*^5.2b", "*0xb*")
+    ])
+    def test_hex_int_format(self, hex_int_datatype, value, format_spec, expected):
+        """Test if the hex int datatype works."""
+
+        assert hex_int_datatype.format(value, format_spec) == expected
