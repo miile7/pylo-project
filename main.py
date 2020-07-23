a560cf9676402110eb501c07f7b96e776ea30ee9
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-07-23 16:38:06 +0200

    Adding debug possibilities to main

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ import pylo.config
 pylo.config.VIEW = pylo.CLIView()
 pylo.config.CONFIGURATION = pylo.IniConfiguration()
 
+# pylo.config.CONFIGURATION.setValue("setup", "microscope-module", "pyjem_microscope.py")
+# pylo.config.CONFIGURATION.setValue("setup", "microscope-class", "PyJEMMicroscope")
+# pylo.config.CONFIGURATION.setValue("setup", "camera-module", "pyjem_camera.py")
+# pylo.config.CONFIGURATION.setValue("setup", "camera-class", "PyJEMCamera")
+
 pylo.execute()
+
+# import pylo.microscopes
+
+# controller = pylo.Controller()
+# controller.microscope = pylo.microscopes.PyJEMMicroscope(controller)
+# cliview = pylo.CLIView()
+# cliview._showCreateMeasurementLoop(controller)
