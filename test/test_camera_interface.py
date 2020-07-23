ef147651ddc4d915674a5b06e02a561658eaf129
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-07-23 16:37:01 +0200

    Fixing camera interface test

--- test/test_camera_interface.py
+++ test/test_camera_interface.py
@@ -7,9 +7,16 @@ if __name__ == "__main__":
 import pytest
 import pylo
 
+class DummyConfiguration(pylo.AbstractConfiguration):
+    def getValue(self, *args, **kwargs):
+        return "DEFAULT_CONFIGURATION_VALUE"
+
+pylo.config.CONFIGURATION = DummyConfiguration()
+
+
 class TestCameraInterface:
     def test_for_not_implemented(self):
-        camera = pylo.cameras.CameraInterface()
+        camera = pylo.cameras.CameraInterface(pylo.Controller())
 
         with pytest.raises(NotImplementedError):
             camera.recordImage()
