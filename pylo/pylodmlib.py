f0d56fc04b5f54c7035900f619a489949aedc193
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-08-20 13:29:22 +0200

    Adding typing hints, adding script prefix and additional requires

--- pylo/pylodmlib.py
+++ pylo/pylodmlib.py
@@ -1,10 +1,27 @@
-import DigitalMicrograph as DM
-import typing
+import os
 import time
+import types
+import typing
 
-# required_files = {}
+try:
+    test_error = ModuleNotFoundError()
+except NameError:
+    # for python <3.6, ModuleNotFound error does not exist
+    # https://docs.python.org/3/library/exceptions.html#ModuleNotFoundError
+    class ModuleNotFoundError(ImportError):
+        pass
 
-def executeDMScript(file_path, synchronized_variables={}):
+try:
+    import DigitalMicrograph as DM
+    outside_mode = False
+except (ModuleNotFoundError, ImportError) as e:
+    # raise RuntimeError("This class can onle be used inside Digital Micrograph.")
+    outside_mode = True
+
+def executeDMScript(file_path: str, 
+                    synchronized_variables: typing.Optional[dict]={}, 
+                    require_before: typing.Optional[typing.List[str]]=[],
+                    script_prefix: typing.Optional[str]=""):
     """Require the given `file_path` and execute the contained script as a 
     dm-script.
 
@@ -20,48 +37,26 @@ def executeDMScript(file_path, synchronized_variables={}):
 
     Parameters
     ----------
-    sync_vars : dict
-        The variables to synchronize, the key is the exact name of the variable 
-        in the dm-script code, the value is the datatype that is used for the 
-        `TagGroupSetTagAs<datatype>()`, `TagGroupGetTagAs<datatype>()`, ... 
-        functions
+    file_path : str
+        The file path of the file to require
+    synchronized_variables : dict, optional
+        The variables to synchronize, the key is the exact name of the 
+        variable in the dm-script code, the value is the datatype that is 
+        used for the `TagGroupSetTagAs<datatype>()`, 
+        `TagGroupGetTagAs<datatype>()`, ...
+    require_before : list of strings, optional
+        File paths of dm-script files to require before requiring the `file`
+    script_prefix : str, optional
+        Script to prefix before the `file_path`
 
     Returns
     -------
     str
         The code to append to the dm-script code
     """
-    # global required_files
 
-    script = DMScriptWrapper(file_path, synchronized_variables)
-    # required_files[file_path] = script
-    
-    return script
-
-# def getDMScriptVariable(file_path, variable_name):
-#     """Get the variable of the of the executed file at the `file_path`.
-
-#     Raises
-#     ------
-#     KeyError
-#         When the `file_path` was not required by the `executeDMScript()` 
-#         function
-
-#     Parameters
-#     ----------
-#     variable_name : str
-#         The name of the variable in the dm-script, has to be registered in 
-#         the `executeDMScript()` function
-    
-#     Returns
-#     -------
-#     mixed
-#         The variable value or None if it does not exist
-#     """
-#     global required_files
-
-#     script = required_files[file_path]
-#     return script.getSyncedVar(variable_name)
+    return DMScriptWrapper(file_path, synchronized_variables, require_before,
+                           script_prefix)
 
 class DMScriptWrapper:
     """Wraps a dm-script.
@@ -79,28 +74,36 @@ class DMScriptWrapper:
         functions
     require_before : list
         A list of file paths to require before requiring the `file_path`
+    script_prefix : str, optional
+        Script to prefix before the `file_path`
     """
 
-    def __init__(self, file_path: str, syncronized_variables: dict, require_before: typing.Optional[typing.List[str]]=[]) -> None:
+    def __init__(self, file_path: str, 
+                 synchronized_variables: typing.Optional[dict]={}, 
+                 require_before: typing.Optional[typing.List[str]]=[],
+                 script_prefix: typing.Optional[str]="") -> None:
         """Initialize the script wrapper.
         
         Parameters
         ----------
         file_path : str
             The file path of the file to require
-        synchronized_variables : dict
+        synchronized_variables : dict, optional
             The variables to synchronize, the key is the exact name of the 
             variable in the dm-script code, the value is the datatype that is 
             used for the `TagGroupSetTagAs<datatype>()`, 
             `TagGroupGetTagAs<datatype>()`, ...
-        require_before : list of strings
+        require_before : list of strings, optional
             File paths of dm-script files to require before requiring the `file`
+        script_prefix : str, optional
+            Script to prefix before the `file_path`
         """
         self.file_path = file_path
         self._creation_time_id = str(round(time.time() * 100))
         self.persistent_tag = "python-dm-communication-" + self._creation_time_id
-        self.syncronized_variables = syncronized_variables
+        self.synchronized_variables = synchronized_variables
         self.require_before = require_before
+        self.script_prefix = script_prefix
     
     def __del__(self) -> None:
         """Desctruct the object."""
@@ -114,48 +117,62 @@ class DMScriptWrapper:
         bool
             Success
         """
+        global outside_mode
+
         prefix = []
         if isinstance(self.require_before, (list, tuple)):
             for fp in self.require_before:
-                with open(fp) as f:
+                with open(fp, "r") as f:
                     prefix.append(f.read())
         
-        with open(self.file_path) as f:
+        with open(self.file_path, "r") as f:
             script = "\n\n".join(prefix) + "\n\n"
+            script += self.script_prefix + "\n\n"
             script += f.read()
         
             script += "\n" + self.getSyncDMCode()
 
-            DM.ExecuteScriptString(script)
+            if outside_mode:
+                print(script)
+                path = os.path.join(os.path.dirname(__file__), "pylodmlib-tmp-script.s")
+                f = open(path, "w+")
+                f.write(script)
+                f.close()
+                
+            else:
+                DM.ExecuteScriptString(script)
+            
             return True
             
         return False
     
-    def __enter__(self):
+    def __enter__(self) -> "DMScriptWrapper":
         """Enter the `with`-block."""
         self.exec()
 
         return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    
+    def __exit__(self, exc_type: typing.Optional[typing.Type[BaseException]],
+                 exc_instance: typing.Optional[BaseException],
+                 exc_traceback: typing.Optional[types.TracebackType]) -> bool:
         """Exit the `with`-block."""
         self.freeAllSyncedVars()
 
-    def __getitem__(self, var):
+    def __getitem__(self, var: typing.Any) -> typing.Any:
         """Get the dm script variable with the `var`."""
         return self.getSyncedVar(var)
 
-    def __iter__(self):
+    def __iter__(self) -> typing.Iterator:
         """Get the iterator to iterate over the dm-script variables."""
-        return self.syncronized_variables.__iter__
+        return self.synchronized_variables.__iter__
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Get the number of synchronized variables."""
-        return len(self.syncronized_variables)
+        return len(self.synchronized_variables)
 
-    def __contains__(self, var):
+    def __contains__(self, var: typing.Any) -> bool:
         """Get whether the `var` is a synchronized dm-script variable."""
-        return var in self.syncronized_variables
+        return var in self.synchronized_variables
     
     def exec(self) -> bool:
         """Execute the code.
@@ -167,7 +184,7 @@ class DMScriptWrapper:
         """
         return self()
 
-    def getSyncDMCode(self):
+    def getSyncDMCode(self) -> str:
         """Get the `dm-script` code that has to be added to the executed code 
         for synchronizing.
 
@@ -194,19 +211,19 @@ class DMScriptWrapper:
             "{tg}_tg.TagGroupSetIndexedTagAs{type}({tg}_index, {var});"
         ))
         
-        dm_script = sync_code_prefix.format(
+        dm_script = [sync_code_prefix.format(
             tg=sync_code_tg_name, pt=self.persistent_tag
-        )
+        )]
         
-        for var_name, var_type in self.syncronized_variables.items():
-            dm_script += sync_code_template.format(
+        for var_name, var_type in self.synchronized_variables.items():
+            dm_script.append(sync_code_template.format(
                 tg=sync_code_tg_name, pt=self.persistent_tag, var=var_name, 
                 type=var_type[0].upper() + var_type[1:].lower()
-            )
+            ))
         
-        return dm_script
+        return "\n" + "\n".join(dm_script)
 
-    def getSyncedVar(self, var_name):
+    def getSyncedVar(self, var_name: str) -> typing.Any:
         """Get the value of the `var_name` dm-script variable.
 
         If the `var_name` is synchronized via the `getSyncDMCode()` function, 
@@ -222,10 +239,12 @@ class DMScriptWrapper:
         mixed
             The variable value
         """
-        if var_name not in self.syncronized_variables:
+        global outside_mode
+
+        if var_name not in self.synchronized_variables or outside_mode:
             return None
         
-        var_type = self.syncronized_variables[var_name]
+        var_type = self.synchronized_variables[var_name]
 
         # UserTags are enough but they are not supported in python :(
         user_tags = DM.GetPersistentTagGroup()
@@ -243,7 +262,7 @@ class DMScriptWrapper:
         
         return None
 
-    def freeAllSyncedVars(self):
+    def freeAllSyncedVars(self) -> None:
         """Delete all synchronization from the persistent tags.
 
         Make sure to always execute this function. Otherwise the persistent 
@@ -252,8 +271,10 @@ class DMScriptWrapper:
         # python way does not work
         # user_tags = DM.GetPersistentTagGroup()
         # user_tags.TagGroupDeleteTagWithLabel(persistent_tag)
+        global outside_mode
 
-        DM.ExecuteScriptString(
-            "GetPersistentTagGroup()." + 
-            "TagGroupDeleteTagWithLabel(\"" + self.persistent_tag + "\");"
-        )
+        if not outside_mode:
+            DM.ExecuteScriptString(
+                "GetPersistentTagGroup()." + 
+                "TagGroupDeleteTagWithLabel(\"" + self.persistent_tag + "\");"
+            )
