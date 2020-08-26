1aac94d4857b10e9bbe31113dad47f060d5bc3a1
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-08-26 17:19:58 +0200

    WIP: Adding dm_main for testing, trying to get DMView running, adding DMView::showError()

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pylo
 import pylo.microscopes
 import pylo.cameras
-import pylo.config
 
-pylo.config.VIEW = pylo.DMView()
+pylo.config
+
+pylo.config.VIEW = pylo.CLIView()
 pylo.config.CONFIGURATION = pylo.IniConfiguration()
 
 # pylo.config.CONFIGURATION.setValue("setup", "microscope-module", "pyjem_microscope.py")
