82c9a688b50fafc74de7940e394da287f33f2cf8
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-11-11 14:59:56 +0100

    Adding tryout test for DMMicroscope

---
+++ test/tryout_dm_microscope.py
@@ -0,0 +1,152 @@
+import DigitalMicrograph as DM
+
+print("Initializing DigitalMicrograph environmnet...");
+
+# the name of the tag is used, this is deleted so it shouldn't matter anyway
+file_tag_name = "__python__file__"
+# the dm-script to execute, double curly brackets are used because of the 
+# python format function
+script = ("\n".join((
+    "DocumentWindow win = GetDocumentWindow(0);",
+    "if(win.WindowIsvalid()){{",
+        "if(win.WindowIsLinkedToFile()){{",
+            "TagGroup tg = GetPersistentTagGroup();",
+            "if(!tg.TagGroupDoesTagExist(\"{tag_name}\")){{",
+                "number index = tg.TagGroupCreateNewLabeledTag(\"{tag_name}\");",
+                "tg.TagGroupSetIndexedTagAsString(index, win.WindowGetCurrentFile());",
+            "}}",
+            "else{{",
+                "tg.TagGroupSetTagAsString(\"{tag_name}\", win.WindowGetCurrentFile());",
+            "}}",
+        "}}",
+    "}}"
+))).format(tag_name=file_tag_name)
+
+# execute the dm script
+DM.ExecuteScriptString(script)
+
+# read from the global tags to get the value to the python script
+if DM.GetPersistentTagGroup():
+    s, __file__ = DM.GetPersistentTagGroup().GetTagAsString(file_tag_name);
+    if s:
+        # delete the created tag again
+        DM.ExecuteScriptString(
+            "GetPersistentTagGroup()." + 
+            "TagGroupDeleteTagWithLabel(\"{}\");".format(file_tag_name)
+        )
+    else:
+        del __file__
+
+try:
+    __file__
+except NameError:
+    # set a default if the __file__ could not be received
+    __file__ = ""
+
+if __file__ != "":
+    import os
+    import sys
+    
+    base_path = str(os.path.dirname(os.path.dirname(__file__)))
+    
+    if base_path not in sys.path:
+        sys.path.insert(0, base_path)
+
+print("Initializing python environment...");
+
+import pprint
+import time
+import random
+import threading
+import importlib
+import traceback
+
+try:
+    import pylo
+    import pylo.microscopes
+    import pylo.cameras
+
+    print("Preparing...");
+
+    pylo = importlib.reload(pylo)
+    pylo.microscopes = importlib.reload(pylo.microscopes)
+    pylo.cameras = importlib.reload(pylo.cameras)
+
+    view = pylo.AbstractView()
+    configuration = pylo.AbstractConfiguration()
+
+    # set configuration
+    from pylo.microscopes.dm_microscope import CONFIG_PYJEM_MICROSCOPE_GROUP
+
+    configuration.setValue("setup", "microscope-module", "dm_microscope.py")
+    configuration.setValue("setup", "microscope-class", "DMMicroscope")
+    configuration.setValue("setup", "camera-module", "dm_camera.py")
+    configuration.setValue("setup", "camera-class", "DMCamera")
+
+    configuration.setValue(CONFIG_PYJEM_MICROSCOPE_GROUP, "focus-calibration", 3)
+    configuration.setValue(CONFIG_PYJEM_MICROSCOPE_GROUP, "objective-lense-coarse-fine-stepwidth", 32)
+    # configuration.setValue(CONFIG_PYJEM_MICROSCOPE_GROUP, "objective-lense-magnetic-field-calibration", None)
+    # configuration.setValue(CONFIG_PYJEM_MICROSCOPE_GROUP, "magnetic-field-unit", None)
+    configuration.removeElement(CONFIG_PYJEM_MICROSCOPE_GROUP, "objective-lense-magnetic-field-calibration")
+    configuration.removeElement(CONFIG_PYJEM_MICROSCOPE_GROUP, "magnetic-field-unit")
+    
+    controller = pylo.Controller(view, configuration)
+
+    pylo.microscopes.DMMicroscope.defineConfigurationOptions(controller.configuration)
+    pylo.cameras.DMCamera.defineConfigurationOptions(controller.configuration)
+
+    controller.microscope = pylo.microscopes.DMMicroscope(controller)
+    controller.camera = pylo.cameras.DMCamera(controller)
+
+    tests = [
+        "set-lorentz-mode",
+        "get-lorentz-mode",
+    ]
+
+    setgettests = {
+        "focus": random.randint(2, 10),
+        "x-tilt": random.randint(2, 10),
+        # "y-tilt": 5,
+        # "ol-current": 0x20,
+    }
+
+    if "set-lorentz-mode" in tests:
+        print("")
+        print("= " * 40)
+        print("")
+        print("Setting microscope into lorentz mode")
+        controller.microscope.setInLorentzMode(True)
+
+    if "get-lorentz-mode" in tests:
+        print("")
+        print("= " * 40)
+        print("")
+        print("Checking if microscope is in lorentz mode")
+        print("In lorentz-mode:", controller.microscope.getInLorentzMode())
+
+    for var_id, value in setgettests.items():
+        print("")
+        print("= " * 40)
+        print("")
+        
+        if var_id == "ol-current":
+            info = " (0x{:x})".format(value)
+        else:
+            info = ""
+        
+        print("Setting {} to {}{}".format(var_id, value, info))
+        controller.microscope.setMeasurementVariableValue(var_id, value)
+
+        print("Getting {}: ".format(var_id), end="")
+        val = controller.microscope.getMeasurementVariableValue(var_id)
+        print(val, end="")
+
+        if var_id == "ol-current":
+            print(" (0x{:x})".format(val), end="")
+        
+        print("")
+
+except Exception as e:
+    print("Exception: ", e)
+    traceback.print_exc()
+    raise e
