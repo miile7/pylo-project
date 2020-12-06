a49f8407376a7d6cc7aa8cc0609e305c307bc750
Maximilian Seidler <maximilian.leonhard.seidler@student.uni-augsburg.de>
2020-12-06 11:09:08 +0100

    Revert "Removing unused DigitalMicrograph module in DMTestCamera", module is used
    This reverts commit dcca86d6629598050d643528baed3a1b4a49f916.

--- devices/dm_test_camera.py
+++ devices/dm_test_camera.py
@@ -2,6 +2,14 @@ import time
 
 import numpy as np
 
+# for python <3.6
+from pylo import FallbackModuleNotFoundError
+
+try:
+    import DigitalMicrograph as DM
+except (FallbackModuleNotFoundError, ImportError) as e:
+    DM = None
+
 # from .dm_camera import DMCamera
 from pylo import loader
 DMCamera = loader.getDeviceClass("Digital Micrograph Camera")
